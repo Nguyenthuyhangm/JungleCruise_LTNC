74454d9b946752b5b92e2cbcf638807c04bc335a
Nguyenthuyhangm <23020662@vnu.edu.vn>
2024-04-25 01:48:18 +0700

    update

--- Text.h
+++ Text.h
@@ -12,6 +12,18 @@ public:
         BLACK_TEXT=2,
 
     };
+    bool LoadFromFile(std::string path);
+    bool LoadFromRenderText(TTF_Font* font,SDL_Renderer* Screen);
+    void Free();
+
+    void SetColor(Uint8 red,Uint8 green,Uint8 blue);
+    void setColor(int type);
+
+    void RenderText(SDL_Renderer* Screen,int xp,int yp,SDL_Rect* clip=NULL,double angle=0.0, SDL_Point* center=NULL,SDL_RendererFlip flip=SDL_FLIP_NONE);
+    int GetWidth()const {return width_;}
+    int GetHeight()const{return height_;}
+
+    void setText (const std::string& text){str_val_}
 private:
     std::string str_val_;
     SDL_Color text_color;
